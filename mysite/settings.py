cc30db974c965e323e0647543efabbef426d0a39
khanhnhanbk <nhanhoa21012002@gmail.com>
2024-07-07 14:47:53 +0700

    chore: Update ALLOWED_HOSTS in settings.py and add wsgi.py alias

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-a8e1^#h#jksehrj()3emdv(x$sap9i+y%7=vd+al85qut2256s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
